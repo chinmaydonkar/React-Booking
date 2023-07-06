59c060774fcdac1da77037139cfdae6126935788
chinmaydonkar <chinmaydonkar@gmail.com>
2023-07-06 13:01:07 +0530

    navbar

---
+++ hotel-booking/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./home.css"
+import Navbar from '../../components/navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div><Navbar/></div>
+  )
+}
+
+export default Home
